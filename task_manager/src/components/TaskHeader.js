aa10c52cb57ca3750fb0f2bee4e55e0e2be93250
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-03-13 17:08:29 +0100

    added crud operations for tasks

--- task_manager/src/components/TaskHeader.js
+++ task_manager/src/components/TaskHeader.js
@@ -1,6 +1,32 @@
-function TaskHeader({ value }){
+function TaskHeader({ task, isEditable,  setIsEditable, editTask }){
+    const { title, id } = task
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        editTask(id, event.target.title.value);
+        setIsEditable(false);
+    };
+
+    if(isEditable){
+        return (
+            <div>
+                <form className="container d-flex flex-row justify-content-between"
+                      onSubmit={onSubmitHandler}
+                >
+                    <input defaultValue={title} className="form-control"
+                           name="title"
+                    />
+                    <button className="btn btn-info m-2">
+                        Edit
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+
     return (
-        <div className="card-title"><h5>{value}</h5></div>
+        <div className="card-title"><h5>{title}</h5></div>
     );
 };
 
