f1a4d04629808514d70b819ddfa52726317f973e
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-03-19 12:04:27 +0100

    added function for setting step as done

--- task_manager/src/components/TaskStep.js
+++ task_manager/src/components/TaskStep.js
@@ -2,17 +2,35 @@ import { useState } from "react";
 
 import EditStepForm from "./EditStepForm";
 
-function TaskStep({ step, deleteStep, updateStep }){
+function TaskStep({ step, deleteStep, updateStep, setStepIsDone }){
     const [showEditStepForm, setEditStepForm] = useState(false);
+
+    const { isDone } = step;
 
     const onDeleteStep = (event) => {
         event.preventDefault();
         deleteStep(step.id);
     };
 
+    const onSetStepIsDone = (event) => {
+        setStepIsDone(step, !isDone);
+    };
+
     return (
         <div className="d-flex flex-row justify-content-between m-1 p-2" style={{backgroundColor: "whitesmoke", width: 500}}>
-            {showEditStepForm ? <EditStepForm step={step} updateStep={updateStep} setEditStepForm={setEditStepForm}/> : step.title}
+            {
+                showEditStepForm ? 
+                    <EditStepForm step={step} updateStep={updateStep} setEditStepForm={setEditStepForm}/> 
+                : 
+                    <div className="d-flex flex-row ">
+                        <input type="checkbox" onChange={onSetStepIsDone} checked={isDone}/>  
+                        <div className="m-1">
+                            {
+                                step.isDone ? (<del>{step.title}</del>) : (step.title)
+                            }
+                        </div>
+                    </div>
+            }
             <div>
                 <a className="m-1 p-1" href="#" onClick={onDeleteStep}>
                     <img src="/delete.png" />
