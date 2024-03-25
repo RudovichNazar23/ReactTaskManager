16964b9f54d52699ace9d5a205bc3d51cf3803e2
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-03-25 08:59:01 +0100

    fixed bugs at task progress rendering

--- task_manager/src/components/StepProgress.js
+++ task_manager/src/components/StepProgress.js
@@ -1,11 +1,13 @@
 
 function StepProgress({ task }){
     const { steps } = task;
+    
+    let progress = 0;
 
-    const doneSteps = steps.filter((step) => step.isDone === true);
-
-    const progress = (doneSteps.length / steps.length) * 100
-
+    if(steps.length > 0){
+        const doneSteps = steps.filter((step) => step.isDone === true);
+        progress = (doneSteps.length / steps.length) * 100
+    }
     return(
         <div>
             <h4 className="" style={{color: "#3366ff"}}>
--- task_manager/src/components/StepStripe.js
+++ task_manager/src/components/StepStripe.js
@@ -2,14 +2,17 @@
 function StepStripe({ task }){
 
     const { steps } = task;
+    console.log(steps)
+    let progress = 0;
 
-    const doneSteps = steps.filter((step) => step.isDone === true);
-
-    const progress = (doneSteps.length / steps.length) * 100;
+    if(steps.length > 0){
+        const doneSteps = steps.filter((step) => step.isDone === true);
+        progress = (doneSteps.length / steps.length) * 100;
+    };
 
     const containerStyle = {
         backgroundColor: "#3366ff",
-        width: `${progress - 10}%`, 
+        width: `${progress}%`, 
         height: "10px",
     };
 
